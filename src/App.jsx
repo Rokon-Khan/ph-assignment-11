398e09e405f49b93cc1bd6b00fb0bcc99a003ecd
Md Rokonuzzaman <rokonjust@gmail.com>
2024-12-22 12:22:35 +0600

    PH Assignment-11 Basic Client Side Setup Done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-center bg-warning py-4">
+        PH Assignrment-11 Start Wow...!!!
+      </h1>
+      <button className="btn btn-primary">Click Me</button>
+    </>
+  );
+}
+
+export default App;

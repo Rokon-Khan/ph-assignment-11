083b43fe26cb45c7dcc3e856222a88275b16d0cb
Rokon Khan <rokonjust@gmail.com>
2024-12-26 07:06:23 +0600

    The Delete Funtionality Added by User in the Frontend

--- src/components/AllAsignmentCard.jsx
+++ src/components/AllAsignmentCard.jsx
@@ -1,8 +1,69 @@
+import { useContext, useState } from "react";
 import { GoArrowUpRight } from "react-icons/go";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+import { AuthContext } from "../authprovider/AuthProvider";
 
 const AllAsignmentCard = ({ assignmentItem }) => {
-  const { _id, assignmentTitle, photo } = assignmentItem || {};
+  const { user } = useContext(AuthContext);
+  const [assignments, setAssignments] = useState([]);
+  const { _id, email, assignmentTitle, photo } = assignmentItem || {};
+
+  const handleDelte = (_id) => {
+    if (user?.email !== email) {
+      // Show the error message before returning
+      Swal.fire({
+        title: "Error",
+        text: "You are not the creator of this assignment.",
+        icon: "error",
+      });
+      return; // Stop further execution
+    }
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`${import.meta.env.VITE_API_URL}/allassignmnets/${_id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount > 0) {
+              Swal.fire({
+                title: "Done!!!",
+                text: "Assignment Deleted Successfully!",
+                icon: "success",
+              });
+              const remaining = assignments.filter(
+                (assign) => assign._id !== _id
+              );
+              setAssignments(remaining);
+            } else {
+              Swal.fire({
+                title: "Error",
+                text: "Failed to delete the Assignment.",
+                icon: "error",
+              });
+            }
+          })
+          .catch((error) => {
+            console.error("Error deleting campaign:", error);
+            Swal.fire({
+              title: "Error",
+              text: "Failed to delete the campaign.",
+              icon: "error",
+            });
+          });
+      }
+    });
+  };
+
   return (
     <div>
       <div className="card card-compact bg-base-100 w-96 shadow-xl">
@@ -18,7 +79,10 @@ const AllAsignmentCard = ({ assignmentItem }) => {
             >
               Update
             </Link>
-            <button className="btn bg-red-500 text-lg font-semibold text-white">
+            <button
+              onClick={() => handleDelte(_id)}
+              className="btn bg-red-500 text-lg font-semibold text-white"
+            >
               Delete
             </button>
           </div>
